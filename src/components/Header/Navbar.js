43d4bb46817944c4a78fbdf1b31d4d5b7e87aa01
md-gulam-quddus <sohag.1984@gmail.com>
2023-01-04 20:41:34 +0100

    Login Page Added

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const menuItems = (
+    <>
+      <NavLink className="btn btn-ghost" to="/">
+        Home
+      </NavLink>
+      <NavLink className="btn btn-ghost" to="/products">
+        Products
+      </NavLink>
+      <NavLink className="btn btn-ghost" to="/wishlist">
+        WishList
+      </NavLink>
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -25,72 +39,19 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menuItems}
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+
+        <Link to="/">
+          <img
+            src="https://d3ulwu8fab47va.cloudfront.net/skin/frontend/default/ubuycom-v1/images/header/logo-ubuy.jpg"
+            alt=""
+          />
+        </Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Get started</a>
+      <div className="navbar-center hidden lg:flex navbar-end">
+        <ul className="menu menu-horizontal px-1">{menuItems}</ul>
       </div>
     </div>
   );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/products",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
